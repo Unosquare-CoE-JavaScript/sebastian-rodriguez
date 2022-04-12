9b37110f275f64fa42ff2db254901cccaba6e776
Sebastian Rodriguez Marin <sebastian.rodriguez@unosquare.com>
2022-04-11 20:41:10 -0500

    node_advanced: redis first step

--- 1_4_course-node-js-advanced/advanced_node_project/routes/blogRoutes.js
+++ 1_4_course-node-js-advanced/advanced_node_project/routes/blogRoutes.js
@@ -3,20 +3,32 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+
+    const cacheBlogs = await client.get(req.user.id);
+
+    if (cacheBlogs) {
+      return res.send(JSON.parse(cacheBlogs));
+    }
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
@@ -25,7 +37,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
